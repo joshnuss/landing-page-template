0da0ee23a6dce2bdea712fb6e134b1b74bfee8ad
Josh Nussbaum <joshnuss@gmail.com>
2021-03-05 22:54:15 -0500

    Initial commit

---
+++ src/server.js
@@ -0,0 +1,27 @@
+import sirv from 'sirv'
+import polka from 'polka'
+import send from '@polka/send-type'
+import compression from 'compression'
+import * as sapper from '@sapper/server'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const { PORT, NODE_ENV } = process.env
+const dev = NODE_ENV === 'development'
+
+function sendMiddleware(req, res, next) {
+  res.send = send.bind(null, res)
+  next()
+}
+
+polka() // You can also use Express
+  .use(
+    compression({ threshold: 0 }),
+    sirv('static', { dev }),
+    sendMiddleware,
+    sapper.middleware()
+  )
+  .listen(PORT, err => {
+    if (err) console.log('error', err)
+  })
